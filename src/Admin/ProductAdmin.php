0ec83e695fe4d0f054a5a0e6c249212b28287480
Oleksandr Hrachov <sanny4rich@gmail.com>
2019-01-28 16:36:09 +0200

    First commit

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -2,39 +2,41 @@
 
 namespace App\Admin;
 
+
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-
-class ProductAdmin
+class ProductAdmin extends AbstractAdmin
 {
     protected function configureListFields(ListMapper $list)
     {
         $list
-            ->addIdentifier('id')
-            ->addIdentifier('name')
-            ->addIdentifier('description')
-            ->addIdentifier('price')
-            ->add('is_top')
-            ->add('category')
-            ->add('image');
-    }
+            -> addIdentifier('name')
+            -> addIdentifier('id')
+            -> addIdentifier('description')
+            -> addIdentifier('price')
+            -> addIdentifier('category')
+            -> addIdentifier('image');
 
+    }
     protected function configureDatagridFilters(DatagridMapper $filter)
     {
         $filter
-            ->add('id')
-            ->add('name');
+            -> add('id')
+            -> add('name')
+            -> add('description')
+            -> add('price');
     }
-
     protected function configureFormFields(FormMapper $form)
     {
-        $form ->add('name')
-              ->add('description')  ;
-
+        $form
+            -> add('name')
+            -> add('description')
+            -> add('price')
+            -> add('category')
+            -> add('isTop' )
+            -> add('image');
     }
-
-
 }
