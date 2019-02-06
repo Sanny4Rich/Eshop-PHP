4d9ba946252190e9968c8850e429099a1163becc
Oleksandr Hrachov <sanny4rich@gmail.com>
2019-02-06 19:19:24 +0200

    Маска для добавления в корзину

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,19 @@ addToCartButtons = $('.js-add-to-cart');
 
 addToCartButtons.on('click', function(event) {
     event.preventDefault();
-
+    mask();
     console.log('Button pressed.');
 
     $.get(event.target.href, function (data) {
         cartInHeader.html(data);
+        unmask();
     });
-})
+});
+
+function mask() {
+    $('body').append('<div class="lmask"><div>')
+}
+
+function unmask() {
+    $('.lmask').remove();
+}
