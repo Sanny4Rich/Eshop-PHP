998d4336016a0100d97b6693bc13d1e993468939
Oleksandr Hrachov <sanny4rich@gmail.com>
2019-02-08 21:13:19 +0200

    Маска для добавления в корзину

---
+++ src/Migrations/Version20190130193558.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190130193558 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB0CFFE9AD6 FOREIGN KEY (orders_id) REFERENCES orders (id)');
+        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB04584665A FOREIGN KEY (product_id) REFERENCES products (id)');
+        $this->addSql('ALTER TABLE orders RENAME INDEX idx_e283f8d8a76ed395 TO IDX_E52FFDEEA76ED395');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE order_items DROP FOREIGN KEY FK_62809DB0CFFE9AD6');
+        $this->addSql('ALTER TABLE order_items DROP FOREIGN KEY FK_62809DB04584665A');
+        $this->addSql('ALTER TABLE orders RENAME INDEX idx_e52ffdeea76ed395 TO IDX_E283F8D8A76ED395');
+    }
+}
